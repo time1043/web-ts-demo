e8b6ce423c897bdb0a00d5712f4f5cf1747a323e
oswin902m <1665434994@qq.com>
2026-05-18 11:03:36 +0800

    intro react ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,61 @@
+import { useState } from "react";
+
 export default function App() {
-  return <div>App</div>;
+  // const [email, setEmail] = useState(""); // infer the type from the initial value
+  const [email, setEmail] = useState<string>(""); // generic type annotation
+  const [password, setPassword] = useState<string>("");
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [gender, setGender] = useState<string>("male");
+
+  return (
+    <main>
+      <h1>Register</h1>
+
+      <form>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <label htmlFor="gender">Gender</label>
+        <select
+          name="gender"
+          id="gender"
+          value={gender}
+          onChange={(e) => setGender(e.target.value)}
+        >
+          <option value="" disabled>
+            Choose your gender
+          </option>
+          <option value="male">Male</option>
+          <option value="female">Female</option>
+          <option value="other">Other</option>
+        </select>
+
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <label htmlFor="confirmPassword">Confirm Password</label>
+        <input
+          type="password"
+          id="confirmPassword"
+          name="confirmPassword"
+          value={confirmPassword}
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
+
+        <button type="submit">Submit</button>
+      </form>
+    </main>
+  );
 }
